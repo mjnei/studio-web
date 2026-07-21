2e8f842524d82df0a2de58829c5b1c36d9aaeed8
0xMichaelRan <michaelran16@gmail.com>
2026-07-21 21:50:22 +0800

    add websocket allowed domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.50.111'],
+  allowedDevOrigins: ['192.168.50.111', '192.168.43.99'],
   async headers() {
     return [
       {
